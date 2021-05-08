5e158e59a36d9b5bad2c2ec8cad6c268c09480f6
Kavin Kumar K <kavin-pt3987@kavin-pt3987.csez.zohocorpin.com>
2021-05-08 12:34:47 +0530

    feat : add button in the index page

--- tests/integration/components/title/title-test.js
+++ tests/integration/components/title/title-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | title', function(hooks) {
+module('Integration | Component | title/title', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Title />`);
+    await render(hbs`<Title::Title />`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      <Title>
+      <Title::Title>
         template block text
-      </Title>
+      </Title::Title>
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
